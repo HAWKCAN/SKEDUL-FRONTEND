7965f47adfb718c0aecc51db4b597e83da418844
ferdiansamp <mohammad.samputra@mhs.unsoed.ac.id>
2025-11-14 22:04:01 +0700

    Update UI Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Header, LeftSide,Lantai } from "../components";
+import { Header, LeftSide, Lantai } from "../components";
 export default function Dashboard() {
   return (
     <div>
@@ -14,25 +14,27 @@ export default function Dashboard() {
           <h1 className="mt-2 text-[36px] font-bold">Dashboard</h1>
           <p className="text-[20px]">Sabtu, 14 November 2025</p>
           <div className="bg-[#ffffff] h-[12vh] border-solid border-1 rounded-md mt-3 mr-5">
-              <table className="w-[100%] h-[70%] text-center text-[18px] mt-3">
-                <tr>
-                  <th>Peminjaman Pending</th>
-                  <th className="border-r border-l">Peminjaman Diterima</th>
-                  <th>Peminjaman Ditolak</th>
-                </tr>
-                <tr>
-                  {/* Ini bisa diisi nilai aktual dari database nya */}
-                  <td>3</td>
-                  <td className="border-r border-l">3</td>
-                  <td>3</td>
-                </tr>
-              </table>
+            <table className="w-[100%] h-[70%] text-center text-[18px] mt-3">
+              <tr>
+                <th>Peminjaman Pending</th>
+                <th className="border-r border-l">Peminjaman Diterima</th>
+                <th>Peminjaman Ditolak</th>
+              </tr>
+              <tr>
+                {/* Ini bisa diisi nilai aktual dari database nya */}
+                <td>3</td>
+                <td className="border-r border-l">3</td>
+                <td>3</td>
+              </tr>
+            </table>
           </div>
 
-          <div className="bg-[#ffffff] border-solid border-1 rounded-md mt-5 mr-5">
-            <h1 className="text-[18px] font-bold ml-2 mt-2">Daftar Pending</h1>
-            <table className="w-[100%] text-center mb-2">
-              <tr>
+
+          <h1 className="text-[20px] font-bold mt-5">Daftar Pending</h1>
+          <div className="bg-[#ffffff] border-solid rounded-md border-1 mr-5 pl-2">
+            {/* <div className="absolute w-[160px] top-[-50px] border-l border-t border-r h-[50px] bg-[#ffffff] flex justify-center  left-[-1px]">   <h1 className=" text-[18px]  font-bold mt-1">Daftar Pending</h1></div> */}
+            <table className="w-[100%] text-center mt-2 mb-2">
+              <tr >
                 <th>No</th>
                 <th>Nama</th>
                 <th>Tanggal Peminjaman</th>
@@ -54,9 +56,9 @@ export default function Dashboard() {
             </table>
           </div>
 
-          <div className="bg-[#ffffff] border-solid border-1 rounded-md mt-5 mr-5">
-            <h1 className="text-[18px] font-bold ml-2 mt-2">Histori Peminjaman</h1>
-            <table className="w-[100%] text-center mb-2">
+          <h1 className="text-[20px] font-bold mt-3">Histori Peminjaman</h1>
+          <div className="bg-[#ffffff] border-solid border-1 rounded-md mr-5 pl-2">
+            <table className="w-[100%] text-center mt-2 mb-2">
               <tr>
                 <th>No</th>
                 <th>Tanggal</th>
