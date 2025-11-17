f99b1569f53e09d948c0363f469d4233aaecca5c
HAWKCAN <farizrahman335577@gmail.com>
2025-11-17 08:42:12 +0700

    fix background login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: ["bg-login"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/index.css", // pastikan CSS ikut di-scan
+  ],
+  safelist: [
+    { pattern: /.*/ }, // paksa semua class tetap disertakan
+  ],
+  theme: { extend: {} },
   plugins: [],
 };
